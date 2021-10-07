dd8d1a95e9bf354146ea1625e3e9489dd36b5846
Feliciano247 <92082317+Feliciano247@users.noreply.github.com>
2021-10-07 05:04:34 -0400

    Add files via upload

---
+++ Information.java
@@ -0,0 +1,21 @@
+/*
+  Program by Mark Feliciano
+  Assignment no#1
+  Date:09/10/2021
+*/
+public class Information{
+	public static void main(String[]args){
+        String fullname="Mark Feliciano";
+        String std_num="2020500483";
+        String Section="IT-2B";    
+        String bday="May 9, 2001";
+        int age= 20;
+        String email="mark.feliciano.d@bulsu.edu.ph";
+        String con_num="09054638557";
+        String Address="Blk 104 Lot7 Melody Plain"+"\n\t\tSan Jose Del Monte Bulacan";
+        String Cvs="Single";
+        System.out.println("\n\t**STUDENT INFORMATION**");   
+        System.out.printf("\n\tFull Name: %s\n\tStudent Number: %s\n\tSection: %s\n\tEmail: %s\n\tContact Number: %s\n\tBirthday: %s\n\tAge: %s\n\tAddress: %s\n\tCivil Status: %s\n",fullname,std_num,Section,email,con_num,bday,age,Address,Cvs);
+       
+    }
+}
